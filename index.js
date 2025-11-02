42f4094dcc3bbb45a6e10a743046524d56d9ffb3
Samuel Olabode <Solabode499@gmail.com>
2025-11-02 16:09:56 -0700

    `Added MongoDB connection and Mongoose dependency to the project`

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/exercise-tracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+app.post('/api/users', async(req, res)=>{
+  const {username} = req.body;
+
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
