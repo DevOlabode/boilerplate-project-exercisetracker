32f5f5b3881b42f09de41b49ab5e9117c5000c55
Samuel Olabode <Solabode499@gmail.com>
2025-11-02 19:58:11 -0700

    `made URLencorded true`

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ db.once('open', () => {
 });
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -22,12 +24,16 @@ app.get('/', (req, res) => {
 
 app.post('/api/users', async(req, res)=>{
   const {username} = req.body;
-  const user = new User(username);
+  if (!username) {
+    return res.status(400).json({ error: 'Username is required' });
+  }
+  const user = new User({username});
   await user.save();
   res.send(user);
 });
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+const port = process.env.PORT || 3000;
+const listener = app.listen(port, () => {
+  console.log('Your app is listening on port ' + port)
 })
